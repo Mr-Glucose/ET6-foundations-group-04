294fe43fc9ecbbb6afb834a2309ca3502608312a
AnaiMurillo <psi.anamurillo@gmail.com>
2025-01-06 19:22:56 -0500

    Add the  iterative approach used in the function

--- solutions/the_tribonacci_sequence/main.py
+++ solutions/the_tribonacci_sequence/main.py
@@ -1,12 +1,21 @@
 def tribonacci(n: int) -> int:
     """
         Calculate the nth Tribonacci number.
+        The objective is to write an efficient function to compute the nth Tribonacci number while handling edge cases and invalid inputs.
 
     The Tribonacci sequence starts as follows:
     T(0) = 0, T(1) = 1, T(2) = 1, and for n >= 3,
     T(n) = T(n-1) + T(n-2) + T(n-3).
 
+    This function utilizes an iterative approach to calculate the Tribonacci sequence.
+    It maintains a list of the last three calculated numbers and iteratively updates this list
+    by adding the sum of the three numbers to the end, effectively simulating the Tribonacci sequence's definition.
+    This iterative method provides efficient calculation without the overhead of recursive function calls.
+
+    Returns the first n numbers of the tribonacci sequence.
+
     Args:
+
         n (int): A non-negative integer representing the index of the Tribonacci number to calculate.
 
     Returns:
